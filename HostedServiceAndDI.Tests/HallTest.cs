ea39fb619c064f660afbd6be0200a08cb9ea8305
Evgeniy Bokov <zheka2001941@gmail.com>
2022-11-01 01:40:50 +0700

    tests for Hall.cs

---
+++ HostedServiceAndDI.Tests/HallTest.cs
@@ -0,0 +1,33 @@
+﻿using HostedServiceAndDI.entity;
+using HostedServiceAndDI.Exceptions;
+using HostedServiceAndDI.service;
+using NUnit.Framework;
+
+namespace HostedServiceAndDI.Tests;
+
+[TestFixture]
+public class HallTest
+{
+    [Test]
+    public void Hall_Get_Next_Contender_Success()
+    {
+        var hall = new Hall(new ContenderGenerator());
+        var contender = hall.GetNextContender();
+        
+        Assert.IsInstanceOf(typeof(Contender), contender);
+    }
+
+    [Test]
+    public void Empty_Hall_Case_Failure()
+    {
+        var hall = new Hall(new ContenderGenerator());
+        var contendersCount = hall.ContendersCount;
+
+        for (int i = 0; i < contendersCount; i++)
+        {
+            hall.GetNextContender();
+        }
+
+        Assert.Throws<EmptyHallException>(() => hall.GetNextContender());
+    }
+}
